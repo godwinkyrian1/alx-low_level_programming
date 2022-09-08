a236bd00c7cc7e20e59c8bbd40ca21622137a746
godwinkyrian1 <godwinkyrian1@gmail.com>
2022-09-08 07:14:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 /* This script is for the puts function */
 
-int main (void)
+int main(void)
 {
-	char *s = " \"Programming is like building a multilingual puzzle ";
+	char *s = "\"Programming is like building a multilingual puzzle";
 	puts(s);
 	
 	return (0);
