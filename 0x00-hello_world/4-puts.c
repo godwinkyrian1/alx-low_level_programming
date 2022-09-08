80674ffc2d0dcbcb7755df0091d7f80c19f56a5a
godwinkyrian1 <godwinkyrian1@gmail.com>
2022-09-08 08:27:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-/**
- * main - Entry point   
- * Description: 'the program's description'
- * Return: Always 0 (Success)
- */
-
+/* Description: 'the program's description' */
 int main(void)
+
 {
-    	char *s = "\"Programming is like building a multilingual puzzle";
+	char *s = "\"Programming is like building a multilingual puzzle";
+	puts(s);
 
-    	puts(s);
+	return (0);
+}
 
-    	return (0);
-}		
