48edefdee24601c55dddf71d6f8bb3f5d518a72c
godwinkyrian1 <godwinkyrian1@gmail.com>
2022-09-08 19:19:16 -0700

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 
 /**
- * main - entry point
- *
- * return: Always 0 (success)
- */
+ *main -entry point
+ *Description:"data type" 
+ *Return:Always 0(success)
+*/
+
 int main(void)
 {
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
-
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
-	return (0);		                         
+int intType;
+float floatType;
+double doubleType;
+char charType;
+printf("Size of int:%zu bytes\n", sizeof(intType));
+printf("Size of float:%zu bytes\n", sizeof(floatType));
+printf("Size of double:%zu bytes\n", sizeof(doubleType));
+printf("Size of char:%zu byte\n", sizeof(charType));
+return (0);		                         
 }
