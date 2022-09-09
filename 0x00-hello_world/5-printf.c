6137b15007bd9d688c8206122e50a780fc71ceaf
godwinkyrian1 <godwinkyrian1@gmail.com>
2022-09-08 17:32:56 -0700

    6-size.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 /**
- *  Return: Always 0 (success)
+ * main - Entry point
+ *
+ * Description: "5-printf.c "
+ * Return: Always 0 (success)
  */  
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * Return: Always 0(success)
+ */ 
+int main(0)
+{
+i	printf("size of char is =%i byte\n", sizeof(char));
+	printf("size of int is=%i bytes\n", sizeof(int));
+	printf("size of long int is=%i bytes\n", sizeof(long int));
+	printf("size of long long int is=%i bytes\n", sizeof(long long int);
+	printf("size of float int is=%i bytes\n", sizeof(float);
+return (0);
+}	
