fead367fd576817e3c2cd62b13d283417b642050
godwinkyrian1 <godwinkyrian1@gmail.com>
2022-09-09 20:13:45 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main-entry
+ * Description:'print reversals alphabet'
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 122; i > 96; i--)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
+}
