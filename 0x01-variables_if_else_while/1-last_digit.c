2c97a50e7552a4282215f46bf0790d415b829efd
godwinkyrian1 <godwinkyrian1@gmail.com>
2022-09-10 04:16:51 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ else if (l == 0)
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0 ", n, l);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 return (0);
 }
