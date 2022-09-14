3bdc61d9e47ed9a6cc093be20c8c28bea4161b3c
godwinkyrian1 <godwinkyrian1@gmail.com>
2022-09-14 15:49:42 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,6 +9,7 @@ int main(void)
 {
 int c;
 long int n1, n2, fn;
+
 n1 = 1;
 n2 = 2;
 printf("%ld, %ld", n1, n2);
@@ -17,7 +18,7 @@ for (c = 0; c < 48; c++)
 fn = n1 + n2;
 printf(", %ld", fn);
 n1 = n2;
-n2 = fn
+n2 = fn;
 }
 printf("\n");
 return (0);
