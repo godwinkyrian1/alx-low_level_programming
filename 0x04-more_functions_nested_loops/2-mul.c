ec630c3df293c3fd2ed25c48aa2211430eaedb83
godwinkyrian1 <godwinkyrian1@gmail.com>
2022-09-16 15:32:05 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+/**
+ * mul - multiplies two ints.
+ * @a:integer.
+ * @b:integer.
+ *
+ * Return: product.
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
