b0daf9e5b05a1f1abe48cfe13372225a441967c5
godwinkyrian1 <godwinkyrian1@gmail.com>
2022-09-16 17:34:19 -0700

    ghp

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
- * print_numbers - prints numbers 0-9
- * 
- * Return: void
- */
+* print_numbers - prints numbers 0-9
+*Return: void 
+*/
 
 void print_numbers(void)
 {
- int c;
-
+	int c;
 for (c = '0'; c <= '9'; c++)
-
- 	{
-	_putchar(c);
- }
-	_putchar('\n');
+{
+_putchar(c);
+}
+_putchar('\n');
 }
