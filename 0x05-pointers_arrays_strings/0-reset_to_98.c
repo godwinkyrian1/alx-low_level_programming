0977357bbe22dd2a366122a5b5b64fa0946fbc92
godwinkyrian1 <godwinkyrian1@gmail.com>
2022-09-20 15:34:19 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- check the code
+ * @n: integer pointer to a variable to be updated
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
